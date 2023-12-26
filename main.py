fc358b617d8bb8ba127eab741bedd1d37866b213
Sergey <malahovsv1999@gmail.com>
2023-12-26 12:27:11 +0700

    create project for labs

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+app = QApplication(sys.argv)
+w = QWidget()
+
+def configuration():
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle("Simple")
+    w.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    configuration()
