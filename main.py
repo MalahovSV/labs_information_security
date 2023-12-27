7eb88aa5c75d5470bddee68983b9de61a96abce5
Sergey <malahovsv1999@gmail.com>
2023-12-27 11:19:31 +0700

    added generate matrix

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 import sys
 from PyQt5.QtWidgets import QWidget, QLabel, QApplication, QLineEdit, QComboBox, QPushButton
+import numpy as np
+
 
 class EncryptionPlayfair:
+    matrix_playfair = []
+    alphabet = []
+
     def __init__(self, key):
-        matrix_playfair = []
+        self.create_ru_alphabet()                   #русский алфавит по порядку
+        self.generate_matrix(str(key).lower())      #формирование матрицы с ключом
+
+
+    def create_ru_alphabet(self):
+        get_letters = lambda start, end: [chr(code_char) for code_char in range(start, end)]
+        self.alphabet = get_letters(1072, 1078)
+        self.alphabet.append(get_letters(1105, 1106)[0])
+        self.alphabet = self.alphabet + get_letters(1078, 1104)
+
+    def generate_matrix(self, key):
         for letter in key:
-            matrix_playfair.append(letter)
-        print(matrix_playfair)
+            if not (letter in self.matrix_playfair) and ord(letter) != 32:
+                self.matrix_playfair.append(letter)
+        for character in self.alphabet:
+            self.matrix_playfair.append(character) if character not in self.matrix_playfair else ''
+        self.matrix_playfair.append('-')
+        self.matrix_playfair.append('1')
+        self.matrix_playfair.append('2')
+        self.matrix_playfair = np.array(self.matrix_playfair).reshape(6, 6)
 
 
 class Example(QWidget):
@@ -15,7 +36,6 @@ class Example(QWidget):
         super().__init__()
 
         self.initUI()
-
 
     def initUI(self):
         encryption_combo = QComboBox(self)
@@ -38,17 +58,13 @@ class Example(QWidget):
         btn = QPushButton("Зашифровать", self)
         btn.move(15, 160)
 
-
-
         self.setGeometry(300, 300, 350, 350)
         self.setWindowTitle('Absolute')
         self.show()
 
 
 if __name__ == '__main__':
-
-    app = QApplication(sys.argv)
-    ex = Example()
-    EncryptionPlayfair("Hello")
-
-    sys.exit(app.exec_())
+    # app = QApplication(sys.argv)
+    # ex = Example()
+    # sys.exit(app.exec_())
+    EncryptionPlayfair("Ехал грека через реку видит грека в реке рак")
